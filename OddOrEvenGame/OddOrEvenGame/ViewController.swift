a7773f5f7905400f5f32f18eee2ea34537a87499
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-01-06 16:34:01 +0900

    기본화면구성

---
+++ OddOrEvenGame/OddOrEvenGame/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  OddOrEvenGame
+//
+//  Created by 한철희 on 1/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var computerBallCountLbl: UILabel!
+    @IBOutlet weak var userBallCountLbl: UILabel!
+    
+    var comBallsCount: Int = 20
+    var userBallsCount: Int = 20
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        computerBallCountLbl.text = String(comBallsCount)
+        userBallCountLbl.text = String(userBallsCount)
+    }
+
+    @IBAction func gameStartPressed(_ sender: Any) {
+        print("게임시작!!")
+    }
+    
+}
+
